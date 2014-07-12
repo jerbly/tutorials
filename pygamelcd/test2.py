9b752db71c88e128a0d3be1c0854cd7472779481
jerbly <jeremyblythe@gmail.com>
2014-07-12 17:54:18 -0400

    Added text

--- pygamelcd/test2.py
+++ pygamelcd/test2.py
@@ -16,6 +16,9 @@ GPIO.setmode(GPIO.BCM)
 for k in button_map.keys():
     GPIO.setup(k, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+#Colours
+WHITE = (255,255,255)
+
 os.putenv('SDL_FBDEV', '/dev/fb1')
 pygame.init()
 pygame.mouse.set_visible(False)
@@ -23,11 +26,15 @@ lcd = pygame.display.set_mode((320, 240))
 lcd.fill((0,0,0))
 pygame.display.update()
 
+font_big = pygame.font.Font(None, 100)
+
 while True:
     # Scan the buttons
     for (k,v) in button_map.items():
         if GPIO.input(k) == False:
             lcd.fill(v)
+            text_surface = font_big.render('%d'%k, True, WHITE)
+            rect = text_surface.get_rect(center=(160,120))
+            lcd.blit(text_surface, rect)
             pygame.display.update()
     sleep(0.1)    
-
