21df49b48e699ed2e56c026697c7d061ac8adab0
jerbly <jeremyblythe@gmail.com>
2014-07-12 16:28:03 -0400

    Added sleep

--- pygamelcd/test1.py
+++ pygamelcd/test1.py
@@ -5,12 +5,14 @@ Created on Jul 12, 2014
 '''
 import pygame
 import os
+from time import sleep
 
 os.putenv('SDL_FBDEV', '/dev/fb1')
 pygame.init()
 lcd = pygame.display.set_mode((320, 240))
 lcd.fill((255,0,0))
 pygame.display.update()
+sleep(1)
 
 pygame.mouse.set_visible(False)
 
